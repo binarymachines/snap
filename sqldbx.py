e4ee94db431ca79044ed131ce9f479b7fb8d5f29
Dexter Taylor <binarymachineshop@gmail.com>
2017-04-12 13:14:52 -0400

    adding logic for sequence-based PK fields

--- sqldbx.py
+++ sqldbx.py
@@ -38,7 +38,10 @@ class SQLDataTypeBuilder(object):
     def add_primary_key_field(self, name, data_type, **kwargs):
         field = {}
         field['name'] = name
-        field['column'] = Column(data_type, primary_key=True, **kwargs)
+        if kwargs.get('sequence'):
+            field['column'] = Column(data_type, Sequence(kwargs['sequence']), primary_key=True)
+        else:
+            field['column'] = Column(data_type, primary_key=True, **kwargs)
         self.fields.append(field)
         return self
 
