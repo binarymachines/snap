0674b7abe5b075b21e467faf89cc1fcc38b5660d
Dexter Taylor <binarymachineshop@gmail.com>
2017-12-06 16:00:45 -0500

    update minor version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from pip.download import PipSession
 
 
 NAME = 'snap'
-VERSION = '0.9.44'
+VERSION = '0.9.45'
 PACKAGES = find_packages(where='src')
 DEPENDENCIES=['docopt',
               'Flask',
