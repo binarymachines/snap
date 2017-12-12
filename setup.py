eb12184939a65254f1be006df321a9cd064c89cc
Dexter Taylor <binarymachineshop@gmail.com>
2017-12-12 15:15:35 -0500

    bugfix: bad exception catch in routegen

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from pip.download import PipSession
 
 
 NAME = 'snap'
-VERSION = '0.9.49'
+VERSION = '0.9.51'
 PACKAGES = find_packages(where='src')
 DEPENDENCIES=['docopt',
               'Flask',
