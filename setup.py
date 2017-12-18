96882b8d59a95c923f1a26457f7cde19a73ce10d
Dexter Taylor <binarymachineshop@gmail.com>
2017-12-17 20:00:00 -0800

    remove legacy print calls; update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from pip.download import PipSession
 
 
 NAME = 'snap'
-VERSION = '0.9.51'
+VERSION = '0.9.52'
 PACKAGES = find_packages(where='src')
 DEPENDENCIES=['docopt',
               'Flask',
