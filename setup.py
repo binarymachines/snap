3193db319770e887d534cf78eeb9c7b86a8eac09
Dexter Taylor <binarymachineshop@gmail.com>
2018-05-08 12:02:55 -0400

    fix import problem introduced by pip 10.x

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ import os
 import re
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from pip.download import PipSession
-
 
 NAME = 'snap'
 VERSION = '0.9.55'
