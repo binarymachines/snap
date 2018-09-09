a5fb15ec586009bf3a987f9c98d158616ac49735
Dexter Taylor <binarymachineshop@gmail.com>
2018-09-09 08:26:46 -0400

    add docopt function decorator

--- snap/cli_tools.py
+++ snap/cli_tools.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python
 
+from docopt import DocoptExit
+
+
+def docopt_cmd(func):
+    """
+    This decorator is used to simplify the try/except block and pass the result
+    of the docopt parsing to the called action.
+    """
+    def fn(self, arg):
+        try:
+            opt = docopt_func(fn.__doc__, arg)
+
+        except DocoptExit as e:
+            # The DocoptExit is thrown when the args do not match.
+            # We print a message to the user and the usage block.
+
+            print('\nPlease specify one or more valid command parameters.')
+            print(e)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here.
+
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn
 
 class constrained_input_value(ContextDecorator):
     def __init__(self, value_predicate_func, cli_prompt, **kwargs):
