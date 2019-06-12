1c8be18b708abd5c73147c8735f8ad125ebf80a7
Dexter Taylor <binarymachineshop@gmail.com>
2019-06-12 09:35:32 -0700

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError: # for pip <= 9.0.3
 
 
 NAME = 'snap'
-VERSION = '0.9.93'
+VERSION = '0.9.94'
 PACKAGES = find_packages(where='.', exclude='tests')
 DEPENDENCIES=['docopt',
               'Flask',
