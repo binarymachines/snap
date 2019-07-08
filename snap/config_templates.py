36bde8d92af7999f191de7f4711d3d95759123d5
Dexter Taylor <binarymachineshop@gmail.com>
2019-07-08 10:18:33 -0700

    fix templates

--- snap/config_templates.py
+++ snap/config_templates.py
@@ -83,9 +83,9 @@ from snap.loggers import request_logger as log
 
 sys.path.append('{{ project_dir }}')
 
-{%- if transform_module %}
+{% if transform_module %}
 import {{ transform_module }} 
-{%- endif %}
+{% endif %}
 
 f_runtime = Flask(__name__)
 
@@ -136,23 +136,23 @@ def {{t.name}}({{ ','.join(t.route_variables) }}):
             log.info(request.headers)
 
         input_data = {}
-        {%- for route_variable in t.route_variables %}
+        {% for route_variable in t.route_variables %}
         input_data['{{ route_variable }}'] = {{ route_variable }}
-        {%- endfor %}
+        {% endfor %}
 
-        {%- if t.methods == "'POST'" %}
+        {% if t.methods == "'POST'" %}
         request.get_data()
         input_data.update(core.map_content(request))
         
         transform_status = xformer.transform('{{ t.name }}', input_data, headers=request.headers)
-        {%- elif t.methods == "'GET'" or t.methods == "'DELETE'" %}                
+        {% elif t.methods == "'GET'" or t.methods == "'DELETE'" %}                
         input_data.update(request.args)
         
         transform_status = xformer.transform('{{ t.name }}',
                                              core.convert_multidict(input_data),
                                              headers=request.headers)
-        {%- else %}
-        {%- endif %}        
+        {% else %}
+        {% endif %}        
         output_mimetype = xformer.target_mimetype_for_transform('{{ t.name }}')
 
         if transform_status.ok:
